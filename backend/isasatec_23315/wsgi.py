3e4aeb0bec668adadd1233f3531853002b8a4870
Crowdbotics <team@crowdbotics.com>
2020-12-11 11:31:26 +0000

    Update models

--- backend/isasatec_23315/wsgi.py
+++ backend/isasatec_23315/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'isasatec_23315.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "isasatec_23315.settings")
 
 application = get_wsgi_application()
